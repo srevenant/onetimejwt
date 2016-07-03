6260f2c4d670d4c4a86a2d2e9f1cf30dace2aad6
s3tqr2w <brandon.gillespie@kuali.co>
2016-07-02 20:38:34 -0600

    updating requirement

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
   name = 'onetimejwt',
   packages = ['onetimejwt'],
-  version = '1.0',
-  install_requires = ['jwt', 'timeinterval'],
+  version = '1.0.2',
+  install_requires = ['pyjwt', 'timeinterval'],
   description = 'Simple one time jwt - server and client bits',
   author = 'Brandon Gillespie',
   author_email = 'bjg-pypi@solv.com',
